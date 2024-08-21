5fb0866dc6f9d265ad32019abbf26a200f10c43a
Tuna Han Bulanık <tunahanbulanik@outlook.com>
2024-08-21 12:42:39 +0300

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+
+namespace W5LP
+{
+    public class Car
+    {
+        private DateTime _productDate;
+        private string _productId;
+        private string _productBrandName;
+        private string _productModel;
+        private string _productColor;
+        private int _productDoorNumber;
+
+        public DateTime ProductDate
+        {
+            get { return _productDate; }
+            set { _productDate = DateTime.Now; }
+        }
+        public string ProductId
+        {
+            get { return _productId; }
+            set { _productId = value; }
+        }
+        public string ProductBrandName
+        {
+            get { return _productBrandName; }
+            set { _productBrandName = value; }
+        }
+        public string ProductModel
+        {
+            get { return _productModel; }
+            set { _productModel = value; }
+        }
+        public string ProductColor
+        {
+            get { return _productColor; }
+            set { _productColor = value; }
+        }
+        public int ProductDoorNumber
+        {
+            get { return _productDoorNumber; }
+            set { _productDoorNumber = value; }
+        }
+
+        public override string ToString()
+        {
+
+            return $"Product date: {ProductDate}\n" +
+                   $"Product ID: {ProductId}\n" +
+                   $"Product brand name: {ProductBrandName}\n" +
+                   $"Product model: {ProductModel}\n" +
+                   $"Product color: {ProductColor}\n" +
+                   $"Product door amount: {ProductDoorNumber}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace W5LP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Car> cars = new List<Car>(); // Our list
+
+            Console.WriteLine("Bir araba üretmek ister misiniz? İstiyorsanız 'e' istemiyorsanız 'h' tuşlamanız yeterli");
+            char userChoose;
+            bool isValidChoose = char.TryParse(Console.ReadLine(), out userChoose);
+
+            if (isValidChoose)
+            {
+                if (userChoose == 'e')
+                {
+                    CarMakingLine:  // For goto command
+
+                    Car newCar = new Car();
+                    newCar.ProductDate = DateTime.Now;
+
+                    Console.WriteLine("Lütfen arabanın ismini girin:");
+                    newCar.ProductId = Console.ReadLine();
+
+                    Console.WriteLine("Lütfen arabanın marka ismini girin:");
+                    newCar.ProductBrandName = Console.ReadLine();
+
+                    Console.WriteLine("Lütfen arabanın modelini girin:");
+                    newCar.ProductModel = Console.ReadLine();
+
+                    Console.WriteLine("Lütfen arabanın rengini girin:");
+                    newCar.ProductColor = Console.ReadLine();
+
+                    DoorNumberRetry:
+
+                    Console.WriteLine("Lütfen arabanın kapı sayısını girin:");
+                    int doorNumber;
+                    while (!int.TryParse(Console.ReadLine(), out doorNumber))
+                    {
+                        Console.WriteLine("Hatalı giriş!");
+                        goto DoorNumberRetry;
+                    }
+                    if (doorNumber != 4 && doorNumber != 2) // Looking for 2 or 4 door amount
+                    {
+                        Console.WriteLine("Hatalı giriş! Kapı sayısı yalnızca 2 veya 4 olabilir.");
+                        goto DoorNumberRetry;
+                    }
+                    else
+                    {
+                        newCar.ProductDoorNumber = doorNumber;
+                    }
+
+
+                    Console.WriteLine("\n Üretilen Araba:");
+                    Console.WriteLine(newCar.ToString());
+
+                    cars.Add(newCar);
+
+                    Console.WriteLine("Başka araç üretmek için 'e' üretmemek için 'h' diyebilirsiniz");
+                    char userSecondChoose = Convert.ToChar(Console.ReadLine());
+
+                    if (userSecondChoose == 'e')
+                    {
+                        goto CarMakingLine;
+                    }
+                    else if (userSecondChoose == 'h')
+                    {
+                        Console.WriteLine("Araba üretimi yapılmadı!");
+                    }
+                }
+                else if (userChoose == 'h')
+                {
+                    Console.WriteLine("Araba üretimi yapılmadı!");
+                }
+            }
+
+            foreach (Car car in cars)
+            {
+                Console.WriteLine("Ürettiğiniz araçlar:");
+                Console.WriteLine(car);
+            }
+        }
+    }
+}
